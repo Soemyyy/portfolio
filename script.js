253d155d34bb54ccf7d14665f38dc86873b929e1
Soemyyy <sarahnouvian@gmail.com>
2026-02-17 11:43:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-// Animation au défilement
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('active');
             const bars = entry.target.querySelectorAll('.progress');
-            bars.forEach(bar => bar.style.width = bar.getAttribute('data-width'));
+            bars.forEach(bar => {
+                bar.style.width = bar.getAttribute('data-width');
+            });
         }
     });
 }, { threshold: 0.1 });
 
 document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
 
-// Gestion Modale
+// Gestion Modale Mentions Légales
 const modal = document.getElementById("legalModal");
 const btn = document.getElementById("openModal");
-const close = document.querySelector(".close-btn");
+const span = document.getElementById("closeModal");
 
-btn.onclick = () => modal.style.display = "block";
-close.onclick = () => modal.style.display = "none";
-window.onclick = (e) => { if(e.target == modal) modal.style.display = "none"; }
+btn.onclick = (e) => { e.preventDefault(); modal.style.display = "block"; }
+span.onclick = () => { modal.style.display = "none"; }
+window.onclick = (e) => { if (e.target == modal) modal.style.display = "none"; }
